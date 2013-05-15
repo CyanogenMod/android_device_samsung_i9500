bcc328914110b4f1b99d9943d50fb4bc5db9fbdf
Daniel Hillenbrand <codeworkx@cyanogenmod.org>
2013-05-15 16:55:43 +0200

    i9500: add rootfs

--- i9500.mk
+++ i9500.mk
@@ -31,6 +31,14 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 PRODUCT_AAPT_CONFIG := normal hdpi xhdpi xxhdpi
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
+# Rootdir
+PRODUCT_COPY_FILES := \
+    $(LOCAL_PATH)/rootdir/fstab.universal5410:root/fstab.universal5410 \
+    $(LOCAL_PATH)/rootdir/init.universal5410.rc:root/init.universal5410.rc \
+    $(LOCAL_PATH)/rootdir/init.universal5410.usb.rc:root/init.universal5410.usb.rc \
+    $(LOCAL_PATH)/rootdir/init.wifi.rc:root/init.wifi.rc \
+    $(LOCAL_PATH)/rootdir/ueventd.universal5410.rc:root/ueventd.universal5410.rc
+
 ifeq ($(TARGET_PREBUILT_KERNEL),)
 	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
 else
