73c918ffe449c1c593727dfa58baa0e91adfe788
Ethan Chen <intervigil@gmail.com>
2013-07-19 13:02:27 -0700

    i9500: clean up boardconfig, add bluetooth, wifi

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,43 +16,78 @@
 
 LOCAL_PATH := device/samsung/i9500
 
+TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
+
 # This variable is set first, so it can be overridden
 # by BoardConfigVendor.mk
 USE_CAMERA_STUB := true
 
-TARGET_ARCH := arm
+BOARD_VENDOR := samsung
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := universal5410
 TARGET_NO_BOOTLOADER := true
+
+# Platform
 TARGET_BOARD_PLATFORM := exynos5
+
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH_VARIANT_CPU := cortex-a15
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
-TARGET_ARCH_VARIANT := armv7-a-neon
-ARCH_ARM_HAVE_TLS_REGISTER := true
-
-TARGET_BOOTLOADER_BOARD_NAME := universal5410
+TARGET_CPU_SMP := true
 
 # Kernel
-BOARD_KERNEL_CMDLINE := console=ttySAC2,115200n8 vmalloc=512M androidboot.console=ttySAC2
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_PAGESIZE := 2048
-
-# Inline kernel building
-TARGET_KERNEL_SOURCE := kernel/samsung/exynos5410
+BOARD_KERNEL_CMDLINE := console=ttySAC2,115200n8 vmalloc=512M androidboot.console=ttySAC2
 TARGET_KERNEL_CONFIG := cyanogenmod_i9500_defconfig
+TARGET_KERNEL_SOURCE := kernel/samsung/exynos5410
 
-# fix this up by examining /proc/mtd on a running device
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+BOARD_BLUEDROID_VENDOR_CONF := $(LOCAL_PATH)/bluetooth/libbt_vndcfg.txt
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
+
+# Graphics
+USE_OPENGL_RENDERER := true
+BOARD_EGL_CFG := $(LOCAL_PATH)/configs/egl.cfg
+
+# Wifi
+BOARD_WLAN_DEVICE                := bcmdhd
+WPA_SUPPLICANT_VERSION           := VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_HOSTAPD_DRIVER             := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_bcmdhd
+WIFI_DRIVER_MODULE_NAME          := "bcmdhd"
+WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/bcmdhd.ko"
+WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/bcmdhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_STA          := "/system/etc/firmware/fw_bcm4335_b0.bin"
+WIFI_DRIVER_FW_PATH_AP           := "/system/etc/firmware/fw_bcm4335_apsta_b0.bin"
+WIFI_DRIVER_FW_PATH_P2P          := "/system/etc/firmware/fw_bcm4335_p2p_b0.bin"
+WIFI_DRIVER_MODULE_ARG           := "firmware_path=/system/etc/firmware/fw_bcm4335_b0.bin nvram_path=/system/etc/calibration"
+WIFI_DRIVER_MODULE_AP_ARG        := "firmware_path=/system/etc/firmware/fw_bcm4335_apsta_b0.bin nvram=/system/etc/calibration"
+WIFI_BAND                        := 802_11_ABG
+
+# Webkit
+ENABLE_WEBGL := true
+
+# Filesystems
 BOARD_BOOTIMAGE_PARTITION_SIZE := 8388608
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 8388608
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2898264064
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 9604956160
 BOARD_FLASH_BLOCK_SIZE := 4096
 
-# Bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
-
 # Recovery
 BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
 BOARD_HAS_NO_SELECT_BUTTON := true
 TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/recovery/recovery.fstab
+TARGET_USERIMAGES_USE_EXT4 := true
 
 # inherit from the proprietary version
 -include vendor/samsung/i9500/BoardConfigVendor.mk
