b12544461233e4bbd47a703b8d6238bf92d431ab
Ethan Chen <intervigil@gmail.com>
2013-08-17 15:15:50 -0700

    i9500: lights: separate button brightness from backlight

--- liblights/lights.c
+++ liblights/lights.c
@@ -151,15 +151,26 @@ static int set_light_backlight(struct light_device_t *dev,
 {
     int err = 0;
     int brightness = rgb_to_brightness(state);
-    int previous_brightness = read_int(PANEL_FILE);
 
     pthread_mutex_lock(&g_lock);
 
     err = write_int(PANEL_FILE, brightness);
 
-    if (!previous_brightness && (brightness > 0)) {
-        err = write_int(BUTTON_FILE, brightness > 0 ? 1 : 2);
-    }
+    pthread_mutex_unlock(&g_lock);
+
+    return err;
+}
+
+static int set_light_buttons(struct light_device_t *dev,
+            const struct light_state_t *state)
+{
+    int err = 0;
+    int on = (state->color & 0x00ffffff);
+
+    pthread_mutex_lock(&g_lock);
+
+    err = write_int(BUTTON_FILE, on ? 1 : 0);
+
     pthread_mutex_unlock(&g_lock);
 
     return err;
@@ -266,6 +277,8 @@ static int open_lights(const struct hw_module_t *module, const char *name,
 
     if (0 == strcmp(LIGHT_ID_BACKLIGHT, name))
         set_light = set_light_backlight;
+    else if (0 == strcmp(LIGHT_ID_BUTTONS, name))
+        set_light = set_light_buttons;
     else if (0 == strcmp(LIGHT_ID_NOTIFICATIONS, name))
         set_light = set_light_leds_notifications;
     else if (0 == strcmp(LIGHT_ID_ATTENTION, name))
