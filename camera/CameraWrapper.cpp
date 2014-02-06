8bbc46a83fd737dc048c639c5c938694483a7697
Ethan Chen <intervigil@gmail.com>
2014-02-05 22:11:19 -0800

    i9500: use vendor class for proprietary camera HAL

--- camera/CameraWrapper.cpp
+++ camera/CameraWrapper.cpp
@@ -85,7 +85,8 @@ static int check_vendor_module()
     if (gVendorModule)
         return 0;
 
-    rv = hw_get_module("vendor-camera", (const hw_module_t**)&gVendorModule);
+    rv = hw_get_module_by_class("camera", "vendor",
+            (const hw_module_t**)&gVendorModule);
     if (rv)
         ALOGE("failed to open vendor camera module");
     return rv;
