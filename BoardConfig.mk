cfb7ef86112797cda320e9a7d16bac835a3b1518
Alberto96 <albertoguanti@gmail.com>
2014-02-09 17:07:50 +0100

    BoardConfig: kernel compiling flags optimizations
    tune kernel for cortex-a15 with neon-vfpv4 fpu support
    
    Change-Id: If6f3cc1cd61be525b73976d6173e47a7d6dc267f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -41,6 +41,8 @@ TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_SMP := true
 TARGET_CPU_VARIANT := cortex-a15
+TARGET_GLOBAL_CFLAGS += -mtune=cortex-a15 -mfpu=neon-vfpv4 -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a15 -mfpu=neon-vfpv4 -mfloat-abi=softfp
 
 # Enable QC's libm optimizations
 TARGET_USE_QCOM_BIONIC_OPTIMIZATION := true
