aa16e64bcebfbe81d8f2d12a16d6edb661c3cb48
Ying Wang <wangying@google.com>
2014-02-12 14:43:06 -0800

    Use LOCAL_MODULE_RELATIVE_PATH to fix multilib build warning.
    Change-Id: I069f9b8de09870bdb18109501ab256fc185f21cf

--- audio/Android.mk
+++ audio/Android.mk
@@ -18,7 +18,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := audio.primary.$(TARGET_BOOTLOADER_BOARD_NAME)
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := audio_hw.c ril_interface.c
