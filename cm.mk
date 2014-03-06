988c103cd3a04956a721555c8b5ef57f5193dde5
Markus Guidry <markus@gu1dry.com>
2014-03-06 17:02:31 -0500

    minor: Align ro.product with stock
    Additionally this updates the fingerprint to I9500XXUFNB3 (4.4.2)
    
      PS2: Fixes the double override of ro.product.model. The 2nd one
      was supposed to override ro.product.device.
    
    Change-Id: I8bc93d81b3b86d2d1e6a47225bacf7a5b612c0e6

--- cm.mk
+++ cm.mk
@@ -12,4 +12,10 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 PRODUCT_NAME := cm_i9500
 PRODUCT_DEVICE := i9500
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=ja3gxx TARGET_DEVICE=ja3g BUILD_FINGERPRINT="samsung/ja3gxx/ja3g:4.3/JSS15J/I9500XXUEMK8:user/release-keys" PRIVATE_BUILD_DESC="ja3gxx-user 4.3 JSS15J I9500XXUEMK8 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_MODEL=GT-I9500 \
+    PRODUCT_NAME=ja3gxx \
+    PRODUCT_DEVICE=ja3g \
+    TARGET_DEVICE=ja3g \
+    BUILD_FINGERPRINT="samsung/ja3gxx/ja3g:4.4.2/KOT49H/I9500XXUFNB3:user/release-keys" \
+    PRIVATE_BUILD_DESC="ja3gxx-user 4.4.2 KOT49H I9500XXUFNB3 release-keys"
