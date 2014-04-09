0c22cb04b9876e4aec4c189c81ca368dd030de39
Alberto96 <albertoguanti@gmail.com>
2014-04-09 16:44:30 +0300

    i9500: add device specific audio effects table
    Change-Id: I7bb834c4b34c59f11eb7b149ce7faba0e83129be

--- i9500.mk
+++ i9500.mk
@@ -40,7 +40,8 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf
+    $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/configs/audio_effects.conf:system/etc/audio_effects.conf
 
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
