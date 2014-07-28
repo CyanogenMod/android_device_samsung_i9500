51610aeede2a0831d1afc443c4e8c244ed4ceabe
Eric Laurent <elaurent@google.com>
2014-07-27 17:23:22 -0700

    audio HAL: add parameters to open stream functions
    Pass device address (and audio source for inputs) to
    open_output_stream() and open_input_stream() audio HAL functions.
    
    Bug: 14815883.
    Change-Id: I9b28d7f2c8ba67b5b5e413b95953a708cf48d046

--- audio/audio_hw.c
+++ audio/audio_hw.c
@@ -1361,7 +1361,8 @@ static int adev_open_output_stream(struct audio_hw_device *dev,
                                    audio_devices_t devices,
                                    audio_output_flags_t flags,
                                    struct audio_config *config,
-                                   struct audio_stream_out **stream_out)
+                                   struct audio_stream_out **stream_out,
+                                   const char *address __unused)
 {
     struct audio_device *adev = (struct audio_device *)dev;
     struct stream_out *out;
@@ -1635,7 +1636,10 @@ static int adev_open_input_stream(struct audio_hw_device *dev,
                                   audio_io_handle_t handle,
                                   audio_devices_t devices,
                                   struct audio_config *config,
-                                  struct audio_stream_in **stream_in)
+                                  struct audio_stream_in **stream_in,
+                                  audio_input_flags_t flags,
+                                  const char *address __unused,
+                                  audio_source_t source __unused)
 {
     struct audio_device *adev = (struct audio_device *)dev;
     struct stream_in *in;
