b3ad4d85bd1dc899c28e10b97e3563d3e5a46047
Glenn Kasten <gkasten@google.com>
2014-07-28 15:14:28 -0700

    i9500: audio: Fix normal latency capture buffer size
    Bug: 16632574
    Change-Id: I669266c27d6f28f3334e1f6ffe3f5e2e2e36c64f

--- audio/audio_hw.c
+++ audio/audio_hw.c
@@ -85,7 +85,7 @@ struct pcm_config pcm_config_deep = {
 struct pcm_config pcm_config_in = {
     .channels = 2,
     .rate = 44100,
-    .period_size = 512,
+    .period_size = 1024,
     .period_count = 2,
     .format = PCM_FORMAT_S16_LE,
 };
