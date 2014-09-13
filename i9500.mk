2b19b2cec1352fc85c91157a150a5cc098bcfa31
Alberto96 <albertoguanti@gmail.com>
2014-09-13 14:00:07 +0300

    i9500: idc: remove useless Atmel stuff
    Change-Id: Idddac7e3b43e025f02cf26cb99847eac1cbaadab

--- i9500.mk
+++ i9500.mk
@@ -83,7 +83,6 @@ PRODUCT_PACKAGES += \
 
 # Keylayouts
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/idc/Atmel_maXTouch_Touchscreen.idc:system/usr/idc/Atmel_maXTouch_Touchscreen.idc \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
     $(LOCAL_PATH)/keylayout/sec_touchkey.kl:system/usr/keylayout/sec_touchkey.kl
 
