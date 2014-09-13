eafc36df80371f9ae12fdf24e2c231555f7fe006
Alberto96 <albertoguanti@gmail.com>
2014-09-13 13:58:39 +0300

    i9500: Add sec_touchscreen.idc
    Change-Id: Ie8338b01bc0eeb0c4d764e741a88ee11fbc2348b

--- i9500.mk
+++ i9500.mk
@@ -83,6 +83,7 @@ PRODUCT_PACKAGES += \
 
 # Keylayouts
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/idc/sec_touchscreen.idc:system/usr/idc/sec_touchscreen.idc \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
     $(LOCAL_PATH)/keylayout/sec_touchkey.kl:system/usr/keylayout/sec_touchkey.kl
 
---
+++ idc/sec_touchscreen.idc
@@ -0,0 +1,4 @@
+touch.blockzone.xMin = 0
+touch.blockzone.yMin = 426
+touch.blockzone.xMax = 1080
+touch.blockzone.yMax = 1920
