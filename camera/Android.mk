43eae70d835b5967be7cdc1b817b94229258cf80
Franco <frapeti@gmail.com>
2014-11-23 19:18:02 -0300

    revert Camera workarounds
    Change-Id: I14cbcb82690c969e24ec47eb524fe0362ae973da

--- camera/Android.mk
+++ camera/Android.mk
@@ -7,9 +7,6 @@ LOCAL_SRC_FILES := \
 LOCAL_SHARED_LIBRARIES := \
     libhardware liblog libcamera_client libutils
 
-LOCAL_C_INCLUDES := \
-    system/media/camera/include
-
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE := camera.universal5410
 LOCAL_MODULE_TAGS := optional
