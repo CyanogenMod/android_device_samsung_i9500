d8ca0727860665c285ff6bd0a79e18d339c5a73b
Faryaab Sheikh <faryaabsheikh@gmail.com>
2015-01-07 16:24:34 -0300

    BoardConfig updates from SMDK5410 Android 4.2.2 BSP, ODROID-XU 25-01-14
    Conflicts:
    	BoardConfig.mk
    
    Change-Id: I3f38f17987a2059d8663d1bdc4a4bd6140ab7c49

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,26 +13,19 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
 LOCAL_PATH := device/samsung/i9500
-
 BOARD_VENDOR := samsung
-
 # Include path
 TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
-
 # Assert
 TARGET_OTA_ASSERT_DEVICE := ja3g,i9500,GT-I9500
-
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := universal5410
 TARGET_NO_BOOTLOADER := true
-
 # Platform
 TARGET_BOARD_PLATFORM := exynos5
 TARGET_SLSI_VARIANT := insignal
 TARGET_SOC := exynos5410
-
 # Architecture
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
@@ -41,87 +34,121 @@ TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_SMP := true
 TARGET_CPU_VARIANT := cortex-a15
-
 # Kernel
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_PAGESIZE := 2048
 TARGET_KERNEL_CONFIG := cyanogenmod_i9500_defconfig
 TARGET_KERNEL_SOURCE := kernel/samsung/exynos5410
-
+# bionic libc options
+ARCH_ARM_USE_MEMCPY_ALIGNMENT := true
+BOARD_MEMCPY_ALIGNMENT := 64
+BOARD_MEMCPY_ALIGN_BOUND := 32768
 # ANT+
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
-
 # Audio
 BOARD_USES_LIBMEDIA_WITH_AUDIOPARAMETER := true
-
 # Bluetooth
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_BCM := true
 BOARD_BLUEDROID_VENDOR_CONF := $(LOCAL_PATH)/bluetooth/libbt_vndcfg.txt
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
-
 # Boot animation
 TARGET_BOOTANIMATION_PRELOAD := true
 TARGET_BOOTANIMATION_TEXTURE_CACHE := true
-
 # Camera
+TARGET_PROVIDES_CAMERA_HAL := true
+USE_DEVICE_SPECIFIC_CAMERA := true
 BOARD_NEEDS_MEMORYHEAPION := true
-BOARD_USE_STOREMETADATA := true
-BOARD_USE_METADATABUFFERTYPE := true
-BOARD_USE_DMA_BUF := true
-BOARD_USE_ANB_OUTBUF_SHARE := true
-BOARD_USE_GSC_RGB_ENCODER := true
-BOARD_USE_IMPROVED_BUFFER := true
 COMMON_GLOBAL_CFLAGS += -DDISABLE_HW_ID_MATCH_CHECK
 COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
 COMMON_GLOBAL_CFLAGS += -DSAMSUNG_CAMERA_HARDWARE
 COMMON_GLOBAL_CFLAGS += -DSAMSUNG_DVFS
-
+# Samsung OpenMAX Video
+BOARD_USE_STOREMETADATA := true
+BOARD_USE_METADATABUFFERTYPE := true
+BOARD_USE_S3D_SUPPORT := true
+BOARD_USE_DMA_BUF := true
+BOARD_USE_ANB_OUTBUF_SHARE := true
+BOARD_USE_GSC_RGB_ENCODER := true
+BOARD_USE_IMPROVED_BUFFER := true
+BOARD_USE_CSC_HW := false
+BOARD_USE_H264_PREPEND_SPS_PPS := false
+BOARD_USE_QOS_CTRL := false
 # CMHW
 BOARD_HARDWARE_CLASS += hardware/samsung/cmhw
-
 # Graphics
 USE_OPENGL_RENDERER := true
+BOARD_USES_HGL := true
 BOARD_EGL_CFG := $(LOCAL_PATH)/configs/egl.cfg
 BOARD_EGL_SYSTEMUI_PBSIZE_HACK := true
 BOARD_EGL_WORKAROUND_BUG_10194508 := true
-BOARD_USE_BGRA_8888 := true
-NUM_FRAMEBUFFER_SURFACE_BUFFERS := 5
-
+NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
+EXYNOS4_ENHANCEMENTS := true
+TARGET_SAMSUNG_GRALLOC_EXTERNAL_USECASES := true
+# egl cache size : 256 * 1024
+MAX_EGL_CACHE_SIZE := 262144
+# Support RSC on PVR
+BOARD_USES_PVR_RSC := true
+# FIMG2D
+BOARD_USES_SKIA_FIMGAPI := true
+BOARD_USES_NEON_BLITANTIH := true
+#SurfaceFlinger
+BOARD_USES_SYNC_MODE_FOR_MEDIA := true
+# HWCServices
+BOARD_USES_HWC_SERVICES := true
+BOARD_USES_PRESENTATION_SUBTITLES := true
+BOARD_USES_CEC := true
+# HDMI
+# BOARD_USES_GSC_VIDEO := true
+# FIMD
+BOARD_USES_FB_PHY_LINEAR := false
+# HEALTH DAEMON (CHARGER) DEFINES
+RED_LED_PATH := "/sys/class/leds/led_r/brightness"
+GREEN_LED_PATH := "/sys/class/leds/led_g/brightness"
+BLUE_LED_PATH := "/sys/class/leds/led_b/brightness"
+BACKLIGHT_PATH := "/sys/class/backlight/panel/brightness"
+CHARGING_ENABLED_PATH := "/sys/class/power_supply/battery/batt_lp_charging"
 # Media
 COMMON_GLOBAL_CFLAGS += -DUSE_NATIVE_SEC_NV12TILED # use format from fw/native
-
-# NFC
-BOARD_NFC_HAL_SUFFIX := universal5410
-
+# Widevine
+COMMON_GLOBAL_CFLAGS += -DWIDEVINE_PLUGIN_PRE_NOTIFY_ERROR -DADD_LEGACY_ACQUIRE_BUFFER_SYMBOL
 # Radio
 BOARD_PROVIDES_LIBRIL := true
 BOARD_MODEM_TYPE := xmm6360
-
 # Wifi
-BOARD_HAVE_SAMSUNG_WIFI          := true
-BOARD_WLAN_DEVICE                := bcmdhd
-WPA_SUPPLICANT_VERSION           := VER_0_8_X
-BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
+BOARD_HAVE_SAMSUNG_WIFI := true
+BOARD_WLAN_DEVICE := bcmdhd
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
-BOARD_HOSTAPD_DRIVER             := NL80211
-BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_bcmdhd
-WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/dhd/parameters/firmware_path"
-WIFI_DRIVER_FW_PATH_STA          := "/system/etc/wifi/bcmdhd_sta.bin"
-WIFI_DRIVER_FW_PATH_AP           := "/system/etc/wifi/bcmdhd_apsta.bin"
-WIFI_DRIVER_FW_PATH_P2P          := "/system/etc/wifi/bcmdhd_p2p.bin"
-WIFI_BAND                        := 802_11_ABG
-
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+WIFI_DRIVER_FW_PATH_PARAM := "/sys/module/dhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_STA := "/system/etc/wifi/bcmdhd_sta.bin"
+WIFI_DRIVER_FW_PATH_AP := "/system/etc/wifi/bcmdhd_apsta.bin"
+WIFI_DRIVER_FW_PATH_P2P := "/system/etc/wifi/bcmdhd_p2p.bin"
+WIFI_BAND := 802_11_ABG
+# EAP PEAP / AKA / SIM AUTH for WPA_SUPPLICANT_8
+CONFIG_EAP_TLS := true
+CONFIG_EAP_TTLS := true
+CONFIG_EAP_PEAP := true
+CONFIG_EAP_MD5 := true
+CONFIG_EAP_MSCHAPV2 := true
+CONFIG_EAP_SIM := true
+CONFIG_EAP_LEAP := true
+CONFIG_EAP_PSK := true
+CONFIG_EAP_AKA := true
+CONFIG_EAP_PWD := true
+CONFIG_EAP_IKEV2 := true
 # Webkit
 ENABLE_WEBGL := true
-
 # Filesystems
 BOARD_BOOTIMAGE_PARTITION_SIZE := 8388608
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 8388608
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 9371648
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2898264064
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 9604956160
+# userdata = 9604939776 = (9604956160 - 16384 <encryption footer>)
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 9604939776
 BOARD_FLASH_BLOCK_SIZE := 4096
-
 # Recovery
 BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
 BOARD_HAS_NO_SELECT_BUTTON := true
@@ -129,26 +156,10 @@ TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/etc/fstab.universal5410
 TARGET_RECOVERY_PIXEL_FORMAT := BGRA_8888
 TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_RECOVERY_SWIPE := true
-
-# SELinux
-BOARD_SEPOLICY_DIRS += \
-    device/samsung/i9500/sepolicy
-
-BOARD_SEPOLICY_UNION := \
-    file_contexts \
-    device.te \
-    domain.te \
-    gpsd.te \
-    mediaserver.te \
-    surfaceflinger.te \
-    system.te
-
 # Charging mode
 BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
 BOARD_BATTERY_DEVICE_NAME := battery
-
 # Releasetools
 #TARGET_RELEASETOOLS_EXTENSIONS := $(LOCAL_PATH)
-
 # inherit from the proprietary version
 -include vendor/samsung/i9500/BoardConfigVendor.mk
