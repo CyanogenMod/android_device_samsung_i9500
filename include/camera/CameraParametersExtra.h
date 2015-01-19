95c90f02688f221b7deabd83d0c29e2c66fef4d9
Alberto96 <albertoguanti@gmail.com>
2015-01-19 14:36:05 +0100

    i9500: initial bringup for L
    - Clean-up the whole device tree
    - Enable Pre-Kitkat audio policy flag
    - Enable Extended Font support
    - Enable legacy Widevine flags (required for DRM)
    - Clean-up unused/deprecated flags
    - Use custom RIL class to fix lots of wakelocks and SOS calls (derivated from Exynos4RIL)
    - Use custom powerhal to fix wake-up delays
    - Use Open-Source healthd paths
    - Update and fix audio effects for L
    - Use temporary workaround to fix in-call voice quality
    - Enable dynamic A2DP Sample Rate
    - Switch back to Bluetooth phone profile (instead of PC)
    - Fix camera compilation on L
    - Force Camera2 API
    - Enable USB Audio
    - Use newer build fingerprint
    - Update media codecs for L
    - Use source dependent wifi overlays
    - Update and fix NFC configs for L
    - Update and optimize overlays for L
    - Update keylayouts for L
    - Add open-source pvrsrvctl & libcorkscrew required by PVR GPU
    - Update fstab for L
    - Reorganize the whole init process 
    - Fix offline-charging on L
    - Clean-up USB init
    - Add USB charging only mode
    - Clean-up Wi-Fi init
    - Fix Data Encryption (further test is required)
    - Optimize stuff here and there
    
    https://github.com/TheMuppets/proprietary_vendor_samsung/pull/556
    
    Not Working: SELinux
    
    Change-Id: I4b5562643e26590514233d2b8ebb7961a4e47860

---
+++ include/camera/CameraParametersExtra.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2014 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "camera/CameraParametersExtraDurationTimer.h"
+
+#define CAMERA_PARAMETERS_EXTRA_C \
+const char CameraParameters::KEY_CITYID[] = "cityid"; \
+const char CameraParameters::KEY_WEATHER[] = "weather"; \
+\
+int CameraParameters::getInt64(const char *key) const { return -1; } \
+extern "C" { \
+    void acquire_dvfs_lock(void) { } \
+    void release_dvfs_lock(void) { } \
+} \
+CAMERA_PARAMETERS_EXTRA_C_DURATION_TIMER \
+\
+/* LAST_LINE OF CAMERA_PARAMETERS_EXTRA_C, every line before this one *MUST* have
+ * a backslash \ at the end of the line or else everything will break.
+ */
+
+#define CAMERA_PARAMETERS_EXTRA_H \
+    static const char KEY_CITYID[]; \
+    static const char KEY_WEATHER[]; \
+    \
+    int getInt64(const char *key) const; \
+    \
+    /* LAST_LINE OF CAMERA_PARAMETERS_EXTRA_H, every line before this one *MUST* have
+     * a backslash \ at the end of the line or else everything will break.
+     */
