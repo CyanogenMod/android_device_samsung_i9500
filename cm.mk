5c162b94edad96db51ace65247336c356ca59d5b
Alberto96 <albertoguanti@gmail.com>
2015-01-20 20:26:56 +0100

    i9500: initial bringup for L
    - Enable Extended Font support
    - Enable legacy Widevine flags (required for DRM)
    - Clean-up unused/deprecated flags
    - Use custom RIL class to fix lots of wakelocks and SOS calls (derivated from Exynos4RIL)
    - Fix DRM support (tested with Netflix)
    - Update GPS and RIL headers for L
    - Use custom PowerHAL to fix wake-up delays
    - Use Open-Source healthd charger and paths
    - Update and fix audio effects for L
    - Enable dynamic A2DP Sample Rate
    - Switch back to Bluetooth phone profile (instead of PC)
    - Fix camera compilation on L
    - Force Camera2 API
    - Update media codecs for L
    - Update and fix NFC configs for L
    - Update and optimize overlays for L
    - Update keylayouts for L
    - Add open-source pvrsrvctl & libcorkscrew required by PVR GPU
    - Update fstab for L
    - Fix offline-charging on L
    - Clean-up USB init
    - Add USB charging only mode
    - Clean-up Wi-Fi init
    
    https://github.com/TheMuppets/proprietary_vendor_samsung/pull/556
    
    Change-Id: I6a95d4ad7e73e314fa31df824267252d7610f83e
    TO-DO: SELinux support

--- cm.mk
+++ cm.mk
@@ -1,8 +1,5 @@
 $(call inherit-product, device/samsung/i9500/full_i9500.mk)
 
-# Inherit some common CM stuff
-$(call inherit-product, vendor/cm/config/gsm.mk)
-
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
