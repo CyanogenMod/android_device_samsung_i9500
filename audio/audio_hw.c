2b23eac3550324d7be541da471cc2c327540dc44
qinghao <qingxianhao@gmail.com>
2015-03-17 12:39:49 +0800

    i9500: audio: rename paths from HANDSET to EARPIECE
    for less confusing between SOUND_AUDIO_PATH_HANDSET and
    SOUND_AUDIO_PATH_HEADSET
    
    Change-Id: I54912992be5b094b0de254d3e4b0d5ed10b046fc

--- audio/audio_hw.c
+++ audio/audio_hw.c
@@ -536,7 +536,7 @@ static void adev_set_call_audio_path(struct audio_device *adev)
             device_type = SOUND_AUDIO_PATH_SPEAKER;
             break;
         case AUDIO_DEVICE_OUT_EARPIECE:
-            device_type = SOUND_AUDIO_PATH_HANDSET;
+            device_type = SOUND_AUDIO_PATH_EARPIECE;
             break;
         case AUDIO_DEVICE_OUT_WIRED_HEADSET:
             device_type = SOUND_AUDIO_PATH_HEADSET;
@@ -555,7 +555,7 @@ static void adev_set_call_audio_path(struct audio_device *adev)
             break;
         default:
             /* if output device isn't supported, use handset by default */
-            device_type = SOUND_AUDIO_PATH_HANDSET;
+            device_type = SOUND_AUDIO_PATH_EARPIECE;
             break;
     }
 
