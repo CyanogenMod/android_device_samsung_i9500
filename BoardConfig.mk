a0d11f1967c8fc816afd9c73337c04ab0ade4957
Ethan Chen <intervigil@gmail.com>
2015-07-01 13:52:11 -0700

    i9500: Remove deprecated flag
    Change-Id: Id50059d7856ada733e91c2c0ace52cb79c1ef1bc

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -39,7 +39,6 @@ TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_ARCH_VARIANT_CPU := cortex-a15
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
-TARGET_CPU_SMP := true
 TARGET_CPU_VARIANT := cortex-a15
 
 # Kernel
