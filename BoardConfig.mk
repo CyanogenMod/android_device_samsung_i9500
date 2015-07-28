8100862ad400da611e5c5a2dae413daef11ba3a9
Alberto96 <albertoguanti@gmail.com>
2015-07-28 16:40:46 +0200

    i9500: update healthd flags for lollipop
    * fix leds while on offline charging mode
    * enable battery percentage while on offline charging mode
    * remove deprecated flags
    
    Change-Id: I21c1460c3ee97f34704996cf1caa9b76b3b507c5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -76,9 +76,13 @@ BOARD_USES_LEGACY_MMAP := true
 TARGET_RELEASE_CPPFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
 
 # Charger
-BOARD_BATTERY_DEVICE_NAME := battery
 BOARD_CHARGER_ENABLE_SUSPEND := true
+BOARD_CHARGER_SHOW_PERCENTAGE := true
 BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
+RED_LED_PATH := "/sys/class/leds/led_r/brightness"
+GREEN_LED_PATH := "/sys/class/leds/led_g/brightness"
+BLUE_LED_PATH := "/sys/class/leds/led_b/brightness"
+BACKLIGHT_PATH := "/sys/class/backlight/panel/brightness"
 CHARGING_ENABLED_PATH := "/sys/class/power_supply/battery/batt_lp_charging"
 
 # CMHW
