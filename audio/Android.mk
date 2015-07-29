a1ffb3ffc0446ddee78188295f045eaff05c514b
Alberto96 <albertoguanti@gmail.com>
2015-07-30 01:35:32 +0200

    i9500: audio: add HDMI support and proper routing
    Change-Id: I78f4c28c1c87f0d5d262d0a8d2535d90b3c06fae

--- audio/Android.mk
+++ audio/Android.mk
@@ -28,10 +28,14 @@ ifeq ($(BOARD_DYNAMIC_WIDEBAND_SWITCHING), false)
 endif
 
 LOCAL_C_INCLUDES += \
+	$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include \
 	external/tinyalsa/include \
 	$(call include-path-for, audio-effects) \
 	$(call include-path-for, audio-utils) \
 	$(call include-path-for, audio-route)
+
+LOCAL_ADDITIONAL_DEPENDENCIES += \
+	$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
 LOCAL_SHARED_LIBRARIES := liblog libcutils libtinyalsa libaudioutils libdl \
 	libaudience_voicefx libaudioroute
