CyanogenMod/android_device_samsung_manta

Audio latency reduction and configuration
Add af.fast_track_multiplier=1 to reduce audio output latency.
This device has low scheduling jitter and can handle this.

Allow period sizes to be configured by properties, for testing.

Change-Id: If37bc5c529cef4f63ddc878c6e428f4a9dd1dd1b

audio/audio_hw.c
@@ -1864,6 +1864,14 @@ static int adev_open(const hw_module_t* module, const char* name,
 
     *device = &adev->hw_device.common;
 
+    char value[PROPERTY_VALUE_MAX];
+    if (property_get("audio_hal.period_size", value, NULL) > 0) {
+        pcm_config.period_size = atoi(value);
+        pcm_config_in.period_size = pcm_config.period_size;
+    }
+    if (property_get("audio_hal.in_period_size", value, NULL) > 0)
+        pcm_config_in.period_size = atoi(value);
+
     return 0;
 }
 

CyanogenMod/android_device_samsung_i9500

audio/audio_hw.c
@@ -1789,6 +1789,14 @@ static int adev_open(const hw_module_t* module, const char* name,
 
     *device = &adev->hw_device.common;
 
+    char value[PROPERTY_VALUE_MAX];
+    if (property_get("audio_hal.period_size", value, NULL) > 0) {
+        pcm_config_fast.period_size = atoi(value);
+        pcm_config_in.period_size = pcm_config_fast.period_size;
+    }
+    if (property_get("audio_hal.in_period_size", value, NULL) > 0)
+        pcm_config_in.period_size = atoi(value);
+
     return 0;
 }
 