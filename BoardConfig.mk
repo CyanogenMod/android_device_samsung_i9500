d30cd99d1c7c61afa42e2bc8c4791f49b56d34cd
Alberto96 <albertoguanti@gmail.com>
2015-09-10 18:55:12 +0200

    i9500: add proper configurable nvram support
    * macloader won't choose the correct nvram for each hw variant without these flags
    
    Change-Id: Id33c03936f01e576f6554d5869f65a657cc0fed3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -146,6 +146,8 @@ BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_bcmdhd
 WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/dhd/parameters/firmware_path"
 WIFI_DRIVER_FW_PATH_STA          := "/system/etc/wifi/bcmdhd_sta.bin"
 WIFI_DRIVER_FW_PATH_AP           := "/system/etc/wifi/bcmdhd_apsta.bin"
+WIFI_DRIVER_NVRAM_PATH_PARAM     := "/sys/module/dhd/parameters/nvram_path"
+WIFI_DRIVER_NVRAM_PATH           := "/system/etc/wifi/nvram_net.txt"
 WIFI_BAND                        := 802_11_ABG
 
 # inherit from the proprietary version
