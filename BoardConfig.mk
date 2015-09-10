1c6350993dbedcf0d59a525058a1401d97648820
Alberto96 <albertoguanti@gmail.com>
2015-09-10 18:54:10 +0200

    i9500: remove useless p2p firmware path
    * Device doesn't actually supply separate P2P firmware.
    
    Change-Id: Ic424e283ae5ed498a0bc2886a0adae5dcacde76e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -146,7 +146,6 @@ BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_bcmdhd
 WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/dhd/parameters/firmware_path"
 WIFI_DRIVER_FW_PATH_STA          := "/system/etc/wifi/bcmdhd_sta.bin"
 WIFI_DRIVER_FW_PATH_AP           := "/system/etc/wifi/bcmdhd_apsta.bin"
-WIFI_DRIVER_FW_PATH_P2P          := "/system/etc/wifi/bcmdhd_p2p.bin"
 WIFI_DRIVER_NVRAM_PATH_PARAM     := "/sys/module/dhd/parameters/nvram_path"
 WIFI_DRIVER_NVRAM_PATH           := "/system/etc/wifi/nvram_net.txt"
 WIFI_BAND                        := 802_11_ABG
